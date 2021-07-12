a5ac1e9d9a0ad36b33e5a09d9628ab4119c9a009
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-07-12 14:40:37 +0200

    Rearrange components, get rid of Main and import Header and Movie into App

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
   <div id="app">
-    <Main />
+    <header>
+      <Header @search="searchMovies" />
+    </header>
+    <div class="container">
+      <div v-for="movie in movieData" :key="movie.id">
+        <Movie :data="movie" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Main from "./components/Main.vue";
+import Header from "./components/Header.vue";
+import Movie from "./components/Movie.vue";
 
 export default {
   name: "App",
   components: {
-    Main,
+    Header,
+    Movie,
+  },
+  data() {
+    return {
+      apiKey: "3fede56bd77f08a65d287138f1c77073",
+      searchMoviesUrl: "https://api.themoviedb.org/3/search/movie?",
+      movieData: [],
+    };
+  },
+  methods: {
+    async searchMovies(userInput) {
+      let resp = await fetch(
+        `${this.searchMoviesUrl}api_key=${this.apiKey}&language=it-IT&query=${userInput}`,
+        {
+          mode: "cors",
+        }
+      );
+      resp = await resp.json();
+      this.movieData = resp.results;
+    },
   },
 };
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 @import "./style/common.scss";
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+header {
+  height: 7vh;
+}
+
+.container {
+  padding: 1vh 2vw;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  gap: 25px;
 }
 </style>
