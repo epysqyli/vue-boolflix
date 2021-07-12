8c981e02a9f6ce26f952aebf65de98cdb31d84df
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-07-12 12:42:50 +0200

    Add api call method, testing on created()

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,28 @@
 <template>
-  <div></div>
+  <section>
+    <div>
+      <input v-model="userInput" type="text" />
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
   name: "Header",
+  data() {
+    return {
+      userInput: "",
+    };
+  },
 };
 </script>
 
 <style scoped lang="scss">
+section {
+  height: 100%;
+  padding: 0 1vw;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Header />
+    <header><Header /></header>
     <Movie />
   </div>
 </template>
@@ -15,9 +15,33 @@ export default {
     Header,
     Movie,
   },
-  props: {},
+  data() {
+    return {
+      apiKey: "3fede56bd77f08a65d287138f1c77073",
+      searchMoviesUrl: "https://api.themoviedb.org/3/search/movie?",
+    };
+  },
+  methods: {
+    async searchMovies() {
+      let resp = await fetch(
+        `${this.searchMoviesUrl}api_key=${this.apiKey}&language=it-IT&query=fast`,
+        {
+          mode: "cors",
+        }
+      );
+      resp = await resp.json();
+      console.log(resp);
+    },
+  },
+  mounted() {
+    this.searchMovies();
+  }
 };
 </script>
 
 <style scoped lang="scss">
+header {
+  height: 7vh;
+  border: 1px solid red;
+}
 </style>
